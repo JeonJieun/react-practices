74076e2468c60823a6c856b0620275af248448a6
JeonJieun <wjn135@naver.com>
2021-11-29 10:58:17 +0900

    20211129

--- 02.component/kanbanboard/src/01/App.js
+++ 02.component/kanbanboard/src/01/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import kanbanBoard from './KanbanBoard';
+import KanbanBoard from './KanbanBoard';
 
 export default function() {
     return (
         <div id='App'>
-            <KanbanBoard />
+          <KanbanBoard />
         </div>
     )
-} 
+}
